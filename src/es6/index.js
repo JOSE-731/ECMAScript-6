dbb81978444892ccfa7c6a2ef12a12ae8ec8f691
Jose David <jose7312000@gmail.com>
2022-09-14 17:43:33 -0500

    Array y Objetos

--- src/es6/index.js
+++ src/es6/index.js
@@ -22,3 +22,28 @@ function test3(nombre = "Jose", pais = "Colombia", edad="250"){
 }
 
 test1()
+
+
+//Objeto
+let info = {
+    'name': 'Jose',
+    'edad': 15,
+    'pais' : "España"
+}
+
+//Antes de Es6 accediamos de esta forma al objeto
+console.log(info.name, info.edad, info.pais)
+
+//Con Es6, empezó la desestructuración una nueva manera de acceder al objeto
+let {name, edad, pais} = info;
+console.log(edad, name, pais);
+
+
+//Array
+let data1 = ["CO", "PE", 4];
+let data2 = ["CO", "PE", 4];
+
+//Con Es6, unir array
+let union = [...data1, ...data2]
+
+console.log(union)
