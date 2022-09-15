29ccadc9a1e5c4e3af125aaa63f1330360419544
Jose David <jose7312000@gmail.com>
2022-09-14 19:00:29 -0500

    desestructuración de objeto, runciones de flecha y promesas

--- src/es6/index.js
+++ src/es6/index.js
@@ -47,3 +47,59 @@ let data2 = ["CO", "PE", 4];
 let union = [...data1, ...data2]
 
 console.log(union)
+
+//Objetos con variables
+let name1 = "Jose";
+let edad1 = 1;
+
+//Antes de Es6
+objt1 = {
+   name: name1,
+   edad: edad1
+};
+
+//Con Es6
+objt2 = {
+    name1, edad1
+}
+
+console.log(objt1)
+
+//Funciones flecha
+//Las funciones flecha (arrow functions) consiste en una función anónima con la siguiente estructura:
+
+//Antes de Es6
+function nombre (parámetros) {
+    return (valorRetornado)
+}
+
+function suma (num1, num2) {
+    return num1 + num2
+}
+
+
+
+//Despues de Es6
+//Función flecha
+const nombre = (parámetros) => {
+    return (valorRetornado)
+}
+
+const suma = (num1, num2) => num1 + num2
+
+//Promesas
+//Una promesa es una forma de manejar el asincronismo en JavaScript.
+
+const promesa = () => {
+    return new Promise((resolve, reject) => {
+        if(true) {
+            resolve( "Se ha resuelto la promesa" );
+        }else {
+            reject( "Se ha rechazado la promesa" );
+        }
+    });
+}
+
+promesa()
+    .then( respuesta => console.log(respuesta) ) //En caso que se ejecute resolve
+    .catch( error => console.log(error) ) //En caso que se ejecute reject
