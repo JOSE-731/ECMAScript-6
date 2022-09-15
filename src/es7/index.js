268665cb96dc27e04e2025779e0f35c1938bf635
Jose David <jose7312000@gmail.com>
2022-09-14 21:03:34 -0500

    Es7

---
+++ src/es7/index.js
@@ -0,0 +1,19 @@
+//Implementacion en Es7
+
+let num = [1,2,3,4];
+
+//En Es7, se agregó el metodo include que permite saber si un elemento existe en un array
+if(num.includes(2)){
+    console.log("Existe el  numero")
+}else{
+    console.log("No existe el  numero")
+}
+
+
+//Elevar potencia
+let base = 80;
+let exponente = 70;
+
+
+let result = base ** exponente;
+console.log(result);
